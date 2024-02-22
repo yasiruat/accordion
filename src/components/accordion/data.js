02c7797c919dbf1c12ee4d7d126c2243772c758b
Yasiru Thanthirige <yasiruat.26@gmail.com>
2024-02-22 23:25:25 +0530

    components folder

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: "1",
+        question: "What are some key features of renewable energy sources?",
+        answer: "Renewable energy sources, such as solar, wind, hydroelectric, and geothermal power, offer sustainability by harnessing naturally replenishing resources. They produce minimal greenhouse gas emissions, contribute to energy independence, and have the potential for decentralized energy production."
+    },
+    {
+        id: "2",
+        question: "How does machine learning differ from traditional programming?",
+        answer: "Traditional programming involves explicitly providing instructions for a computer to follow, whereas machine learning enables computers to learn from data and make predictions or decisions without being explicitly programmed for each task. Machine learning algorithms improve their performance over time as they are exposed to more data."
+    },
+    {
+        id: "3",
+        question: "What are the main benefits of incorporating mindfulness practices into daily life?",
+        answer: "Mindfulness practices, such as meditation and mindful breathing, offer numerous benefits including stress reduction, improved focus and concentration, increased self-awareness, better emotional regulation, and enhanced overall well-being. Regular mindfulness practice can also promote better sleep and reduce symptoms of anxiety and depression."
+    },
+    {
+        id: "4",
+        question: "How does blockchain technology ensure security in digital transactions?",
+        answer: "Blockchain technology ensures security in digital transactions through decentralization, cryptographic encryption, and immutability. Transactions are recorded in a decentralized ledger across multiple nodes, making it extremely difficult for a single entity to manipulate the data. Cryptographic techniques secure the integrity and confidentiality of transactions, while the immutability of the blockchain prevents tampering or altering of transaction records."
+    }
+]
+
+export default data;
---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiselection(getCurrentId) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection
+          ? "Disable Multiple Selection"
+          : "Enable Multiple Selection"}
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() =>
+                  enableMultiSelection
+                    ? handleMultiselection(dataItem.id)
+                    : handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>No Data Found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
